72b623d37568cdb28b094ecf959b0a39dcad2a3c
manojdrdo <manojdrdo@gmail.com>
2017-08-06 07:26:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.sendFile('article one will be served');
+  res.send('article one will be served');
 });
 
 app.get('/ui/style.css', function (req, res) {
